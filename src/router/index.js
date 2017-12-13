3e1ac78a01673a7448bc26378b9bdad7706417df
wobushirunning <wuhoudesun@126.com>
2017-12-13 18:19:16 +0800

    itemStart

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/views/login'
+import Dailycheck from '@/views/dailycheck'
+import Digitmap from '@/views/digitmap'
+import MainView from '@/views/MainView'
+import Selfhelpbank from '@/views/selfhelpbank'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [{
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: { requiresAuth: true }
+  }, {
+    path: '/',
+    name: '主页',
+    component: MainView
+  }, {
+    path: '/dailycheck',
+    name: '每日检查',
+    component: Dailycheck
+  }, {
+    path: '/digitmap',
+    name: '电子地图',
+    component: Digitmap
+  }, {
+    path: '/selfhelpbank',
+    name: '自助银行',
+    component: Selfhelpbank
+  }]
+})
+
+export default router
